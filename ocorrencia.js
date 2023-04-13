de53c26288bd1a47e92b0b538ff787e48f67cf59
Bruno Saldanha <bsaldanha1990@gmail.com>
2023-04-13 15:02:46 -0300

    correção

--- ocorrencia.js
+++ ocorrencia.js
@@ -3,9 +3,13 @@ function calcularChance(ocorrencias, possibilidades) {
     return chance.toFixed(2) + "%";
   }
   
-  //const ocorrencias = parseInt(prompt("Com quantos resultados você ganha?"));;
-  //const possibilidades = parseInt(prompt("Quantas possibilidades tem: Ex: vitória, derrota e empate:"));;
+  document.addEventListener("DOMContentLoaded", function() {
+    const button = document.querySelector("[name=submit]");
+    button.onclick = function(){
+      const ocorrencias = parseFloat(document.querySelector("#ocorrencias").value);
+      const possibilidades = parseFloat(document.querySelector("#possibilidades").value);
+      const chanceDeOcorrer = calcularChance(ocorrencias, possibilidades);
+      alert(`A chance de vencer é ${chanceDeOcorrer}`);
+    };
+  });
   
-  const chanceDeOcorrer = calcularChance(ocorrencias, possibilidades);
-  
-  //alert(`A chance de vencer é ${chanceDeOcorrer}`);
